dab26d789b1a579d04783bab56f1ed6819ccb8a0
Ruth Chirinos <ruth.chirinos@gmail.com>
2015-09-27 21:45:23 -0400

    comment checkConnectionWithFacebook

--- app/src/main/java/com/delicoin/android/delicoin3/MainActivity.java
+++ app/src/main/java/com/delicoin/android/delicoin3/MainActivity.java
@@ -3,9 +3,12 @@ package com.delicoin.android.delicoin3;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.widget.TextView;
 
@@ -16,6 +19,9 @@ import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 /*
 * http://code.tutsplus.com/es/tutorials/quick-tip-add-facebook-login-to-your-android-app--cms-23837
@@ -42,21 +48,7 @@ public class MainActivity extends Activity {
         //callbackManager = CallbackManager.Factory.create();
 
         /*---*/
-//        try {
-//            PackageInfo info = getPackageManager().getPackageInfo(
-//                    "com.delicoin.android.delicoin3",
-//                    PackageManager.GET_SIGNATURES);
-//            for (Signature signature : info.signatures) {
-//                MessageDigest md = MessageDigest.getInstance("SHA");
-//                md.update(signature.toByteArray());
-//                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
-//            }
-//        } catch (PackageManager.NameNotFoundException e) {
-//            e.printStackTrace();
-//
-//        } catch (NoSuchAlgorithmException e) {
-//            e.printStackTrace();
-//        }
+        //checkConnectionWithFacebook();
         /*---*/
 
 
@@ -70,7 +62,7 @@ public class MainActivity extends Activity {
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-                Intent i = new Intent(MainActivity.this, MainScreen.class);
+                Intent i = new Intent(MainActivity.this, Menu.class);
                 startActivity(i);
                 System.out.print("Logged in");
 //                info.setText(
@@ -96,6 +88,27 @@ public class MainActivity extends Activity {
         });
 
 
+    }
+
+
+    //Check connection
+    public void checkConnectionWithFacebook()
+    {
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "com.delicoin.android.delicoin3",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
